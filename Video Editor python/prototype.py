b35fe7c5e847a6fa943f80841f0f9eba67c0ed06
TonyAMO <aojeda4211@gmail.com>
2022-12-09 11:28:42 -0800

    Speed
    fixed speed window

--- Video Editor python/prototype.py
+++ Video Editor python/prototype.py
@@ -7,26 +7,32 @@ from file import openFile
 
 #Functions
 
-
+final_clip= None
 
 
 def import_clip():
     return VideoFileClip(openFile())
 
 def mix():
+    global final_clip
     mix_input = Tk()
+
     mix_input.title("Mix videos")
     mix_input.geometry("600x400")
     label1 = tkinter.Label(mix_input, text="Enter amount of videos to mix", font=('calibre',10, 'bold'))
     label1.pack()
+    def submit():
+        global i
+        e = entry1.get()
+        print(e)
+        i = int(e)
+        print(i)
     entry1 = tkinter.Entry(mix_input, width=35)
     entry1.pack()
-    def submit():
-        i = entry1.get()
-    button = Button(mix_input, text="Submit", command=lambda: [submit(), mix_input.destroy()])
+    button = Button(mix_input, text="Submit", command=lambda: [submit(), mix_input.destroy(), mix_input.quit()])
     button.pack()
-
     mix_input.mainloop()
+    print(i)
     # clip_1 = import_clip()
     # clip_2 = import_clip()
     # clip_3 = import_clip()
@@ -35,7 +41,7 @@ def mix():
     for c in range(0,i):
         clips.append(import_clip())
     final_clip=concatenate_videoclips(clips)
-    final_clip.write_videofile("final_render.mp4")
+
 
 def mirror():
     clip_mirror = import_clip().fx(vfx.mirror_x)
@@ -50,9 +56,26 @@ def resize():
     clip_resize.write_videofile("final_render.mp4")
 
 def speed_vfx():
-    speed = float(input("Enter your speed: "))
-    clip_speed = import_clip().fx(vfx.speedx, speed)
-    clip_speed.write_videofile("final_render.mp4")
+    global final_clip
+    spd_input = Tk()
+
+    spd_input.title("Speed up/down video")
+    spd_input.geometry("600x400")
+    label1 = tkinter.Label(spd_input, text="Enter speed", font=('calibre', 10, 'bold'))
+    label1.pack()
+    sp = 1
+    def submit():
+        global sp
+        e = entry1.get()
+        print(e)
+        sp = float(e)
+
+    entry1 = tkinter.Entry(spd_input, width=35)
+    entry1.pack()
+    button = Button(spd_input, text="Submit", command=lambda: [submit(), spd_input.destroy(), spd_input.quit()])
+    button.pack()
+    spd_input.mainloop()
+    final_clip = import_clip().fx(vfx.speedx, sp)
 
 def color_vfx():
     color = float(input("Value of darkness: "))
@@ -72,6 +95,28 @@ def audio_file():
     final_clip = videoclip.set_audio(audioclip)
     final_clip.write_videofile("final_render.mp4")
 
+def export():
+    global final_clip
+    exp_input = Tk()
+
+    exp_input.title("Export video")
+    exp_input.geometry("600x400")
+    label1 = tkinter.Label(exp_input, text="Enter name of video", font=('calibre', 10, 'bold'))
+    label1.pack()
+    s = ""
+    def submit():
+        global s
+        e = entry1.get()
+        s = str(e)+".mp4"
+
+
+    entry1 = tkinter.Entry(exp_input, width=35)
+    entry1.pack()
+    button = Button(exp_input, text="Submit", command=lambda: [submit(), exp_input.destroy(), exp_input.quit()])
+    button.pack()
+    exp_input.mainloop()
+    final_clip.write_videofile(s)
+
 #main screen
 
 root = Tk()
@@ -79,14 +124,13 @@ root.title("Video editor")
 # width = root.winfo_screenwidth()
 # height = root.winfo_screenheight()
 # root.geometry('%dx%d' % (width, height))
-root.geometry("750x200")
-root.minsize(750,200)
-root.maxsize(750,200)
+root.geometry("850x200")
+root.minsize(850,200)
+root.maxsize(850,200)
 root.config(bg='#232323')
 
 
 #mix
-
 b=Button(root, text="Mix", relief=GROOVE, bg="#232323", fg="white", command=mix)
 b.pack(side="left", padx=20)
 b.config(width=8, height=3)
@@ -126,5 +170,13 @@ b.config(width=8, height=3)
 b=Button(root, text="Audio", relief=GROOVE, bg="#232323", fg="white", command=audio_file)
 b.pack(side="left", padx=20)
 b.config(width=8, height=3)
+
+#export
+
+b=Button(root, text="Export", relief=GROOVE, bg="#232323", fg="white", command=export)
+b.pack(side="left", padx=20)
+b.config(width=8, height=3)
+
+
 
 root.mainloop()
